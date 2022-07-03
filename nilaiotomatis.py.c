718375fb6047b658318db7811cdf68c881279461
GabutCrypto <107311331+GabutCrypto@users.noreply.github.com>
2022-07-03 22:03:57 +0700

    Add files via upload

---
+++ nilaiotomatis.py.c
@@ -0,0 +1,16 @@
+
+nama = input ("masukan nama...:")
+nilai = input ("masukan nilai..:")
+
+if nilai == "80":
+    print ("\nwah",(nama),"kamu dapat nilai : A")
+    print ("selamat ya",(nama),"atas pencapayannya")
+    print ("lebih semangat lagi beljarnya")
+elif nilai == "60":
+    print ("\nwah",(nama),"kamu dapat nilai : B")
+    print ("selamat ya",(nama),"atas pencapayannya")
+    print ("lebih semangat lagi beljarnya")
+elif nilai == "50":
+    print ("\naduh",(nama),"kamu dapat nilai : C")
+    print ("selamat ya",(nama),"atas pencapayannya")
+    print ("lebih semangat lagi beljarnya")
