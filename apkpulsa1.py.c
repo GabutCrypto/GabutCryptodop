544d735b9488dccc3dab6f5bbc721f75476aa9f6
GabutCrypto <107311331+GabutCrypto@users.noreply.github.com>
2022-07-28 12:13:40 +0700

    Add files via upload

---
+++ apkpulsa1.py.c
@@ -0,0 +1,20 @@
+print ("ISI PULSA")
+print ("1. XL\n""2. exis\n""3. TELKOMSEL")
+
+beli = input ("\nmau beli pulsa apa :")
+
+print ("\npulsa ",(beli)," 5.000 = 4k")
+
+bayar = input ("Pilih metode pembayaran\n\n•Dana •Ovo :")
+
+
+print ( (bayar),"= masukan jumlah")
+
+uang = input ("Bayar :")
+
+print ((uang), "apakah suada nenar")
+
+input ("....?")
+
+print ("selamat isi pulsa anda sudah berhasil")
+print ("TERIMA KASIH")
